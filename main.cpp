b4a24ea8d06cadad10eaa104c6c4812a9f983d38
caleb <caleb.a.mccarthy@gmail.com>
2020-06-17 08:23:17 -0500

    Make server async

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
-
 #include <ctime>
+#include <iostream>
 #include <string>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
 #include <boost/array.hpp>
+
+#include <boost/enable_shared_from_this.hpp>
 #include <boost/asio.hpp>
 
 using boost::asio::ip::tcp;
@@ -13,32 +16,96 @@ std::string make_daytime_string()
   time_t now = time(0);
   return ctime(&now);
 }
-int main(int argc, char **argv) {
-     try
+
+class tcp_connection
+  : public boost::enable_shared_from_this<tcp_connection>
+{
+public:
+  typedef boost::shared_ptr<tcp_connection> pointer;
+
+  static pointer create(boost::asio::io_context& io_context)
   {
-    boost::asio::io_context io_context;
+    return pointer(new tcp_connection(io_context));
+  }
 
-    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 14));
+  tcp::socket& socket()
+  {
+    return socket_;
+  }
 
-    for (;;)
-    {
-      tcp::socket socket(io_context);
-      acceptor.accept(socket);
-
-      std::cout<<"sending message!"<<std::endl;
-      std::string message = make_daytime_string();
-      
-      boost::array<char, 256> buf;
+  void start()
+  {
+    message_ = "Received\n";
+    boost::array<char, 256> buf;
       boost::system::error_code error;
 
-      size_t len = socket.read_some(boost::asio::buffer(buf), error);
-      std::cout<<"length of read string:"<<len<<std::endl;      
-        
+      size_t len = socket_.read_some(boost::asio::buffer(buf), error);
+      std::cout<<"length of read string:"<<len<<std::endl; 
+      
       std::cout.write(buf.data(), len);
 
-      boost::system::error_code ignored_error;
-      boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+    boost::asio::async_write(socket_, boost::asio::buffer(message_),
+        boost::bind(&tcp_connection::handle_write, shared_from_this(),
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred));
+  }
+
+private:
+  tcp_connection(boost::asio::io_context& io_context)
+    : socket_(io_context)
+  {
+  }
+
+  void handle_write(const boost::system::error_code& /*error*/,
+      size_t /*bytes_transferred*/)
+  {
+  }
+
+  tcp::socket socket_;
+  std::string message_;
+};
+
+class tcp_server
+{
+public:
+  tcp_server(boost::asio::io_context& io_context)
+    : acceptor_(io_context, tcp::endpoint(tcp::v4(), 1026))
+  {
+    start_accept();
+  }
+
+private:
+  void start_accept()
+  {
+    tcp_connection::pointer new_connection =
+      tcp_connection::create(acceptor_.get_executor().context());
+
+    acceptor_.async_accept(new_connection->socket(),
+        boost::bind(&tcp_server::handle_accept, this, new_connection,
+          boost::asio::placeholders::error));
+  }
+
+  void handle_accept(tcp_connection::pointer new_connection,
+      const boost::system::error_code& error)
+  {
+    if (!error)
+    {
+      new_connection->start();
     }
+
+    start_accept();
+  }
+
+  tcp::acceptor acceptor_;
+};
+
+int main()
+{
+  try
+  {
+    boost::asio::io_context io_context;
+    tcp_server server(io_context);
+    io_context.run();
   }
   catch (std::exception& e)
   {
